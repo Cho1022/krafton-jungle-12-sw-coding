bcef2aae422ecab4605df7ba5da24715a24268f1
Cho1022 <cbs001022@gmail.com>
2026-03-07 22:48:49 +0900

    week2 basic dict, array 풀이

--- week2/basic/01_python_dict.py
+++ week2/basic/01_python_dict.py
@@ -42,15 +42,32 @@ def find_above_average_students(students):
         tuple: (평균 점수, 평균 이상 학생 이름 리스트)
     """
     # TODO: 모든 학생의 점수를 리스트로 추출하세요
-    pass
-    
+    scores = []
+    for student in students1: 
+        scores.append(student["score"])
+
     # TODO: 평균 점수를 계산하세요
     pass
-    
+
     # TODO: 평균 이상인 학생들의 이름을 리스트로 추출하세요
     pass
-    
+
+    # 1. 모든 학생의 점수 추출
+    scores = []
+    for student in students: # students1 대신 함수가 받은 students 사용
+        scores.append(student["score"])
+
+    # 2. 평균 점수 계산
+    average = sum(scores) / len(scores)
+
+    # 3. 평균 이상인 학생 이름 추출
+    above_average_students = []
+    for student in students:
+        if student["score"] >= average:
+            above_average_students.append(student["name"])
+            
     return average, above_average_students
+    
 
 # 테스트 케이스
 if __name__ == "__main__":
