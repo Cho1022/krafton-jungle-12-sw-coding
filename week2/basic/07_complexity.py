2ba422d98133bd1a3ab214367f2c6e8e3608871f
Cho1022 <cbs001022@gmail.com>
2026-03-10 20:39:59 +0900

    시간복잡도&공간복잡도 close #13

--- week2/basic/07_complexity.py
+++ week2/basic/07_complexity.py
@@ -35,7 +35,10 @@ def find_duplicates_brute_force(nums):
     ## i번째 원소와 i+1 이후의 모든 원소를 비교
     ## 같은 원소를 찾으면 duplicates에 추가 (중복 추가 방지 필요)
     pass
-    
+    for i in range(0, n -1):
+        for j in range(i + 1 , n):
+            if nums[i] == nums[j] and nums[i] not in duplicates:
+                duplicates.append(nums[i])       
     return duplicates
 
 def find_duplicates_sorting(nums):
@@ -49,13 +52,17 @@ def find_duplicates_sorting(nums):
     
     # TODO: 배열을 정렬하세요 (nums.sort() 사용)
     pass
-    
     duplicates = []
-    
+    nums.sort()
     # TODO: 인접한 원소를 비교하여 중복 찾기
     # i와 i+1 원소가 같고, duplicates에 없으면 추가
     pass
-    
+    n = len(nums)
+
+    for i in range(1,n):
+        if nums[i] == nums[i-1]:
+            if nums[i] not in duplicates: 
+               duplicates.append(nums[i])    
     return duplicates
 
 def find_duplicates_hash(nums):
@@ -66,12 +73,19 @@ def find_duplicates_hash(nums):
     """
     seen = set()
     duplicates = set()
-    
+
     # TODO: 각 원소를 순회하면서
     ## 이미 seen에 있으면 duplicates에 추가
     ## 없으면 seen에 추가
     pass
-    
+    for num in nums:
+        if num in seen:
+            duplicates.add(num)
+        else:
+            seen.add(num)
+
+
+
     return list(duplicates)
 
 def measure_time(func, nums, method_name):
