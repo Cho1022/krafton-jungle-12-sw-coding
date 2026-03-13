be4ddbda6517ecae543dc5a74bbea67a62cbb5e5
Cho1022 <cbs001022@gmail.com>
2026-03-13 21:38:39 +0900

    백준4344 close #18

--- week2/problem-solving/난이도하_배열_평균은넘겠지_브론즈1.py
+++ week2/problem-solving/난이도하_배열_평균은넘겠지_브론즈1.py
@@ -1,2 +1,23 @@
 # 배열 - 평균은 넘겠지 (백준 브론즈1)
 # 문제 링크: https://www.acmicpc.net/problem/4344
+
+# 1. 맨 처음 주어지는 테스트 케이스 개수를 받습니다.
+C = int(input()) 
+
+# 2. C번만큼 회원님의 코드를 반복 실행합니다.
+for _ in range(C):
+   
+    data = list(map(int, input().split()))
+    N = data[0] # N은 학생 수입니다. (첫 번째 숫자)
+    scores = data[1:] # scores는 학생들의 점수입니다. (두 번째 숫자부터 끝까지)
+    
+    average = sum(scores) / len(scores)
+    
+    count = 0
+    for num in scores:
+        if num > average:
+            count += 1
+            
+    # TODO: 방금 말씀드린 곱하기 100을 적용해서 rate 식을 완성해 보세요!
+    rate = (count / N) * 1000
+    print(f"{rate:.3f}%")
