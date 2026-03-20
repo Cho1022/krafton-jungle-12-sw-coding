f635ec5540677f7bf2c6afaf9dd52e421e047874
Cho1022 <cbs001022@gmail.com>
2026-03-20 19:15:06 +0900

    이진 트리 close #78

--- week4/basic/02_bst.py
+++ week4/basic/02_bst.py
@@ -52,11 +52,20 @@ def search_bst(root, target):
     """
     # TODO: root가 None이면 False 반환
     pass
-    
+    if root is None:
+        return False
     # TODO: 값을 찾으면 True 반환
+    if root == target:
+        return True
     ## target이 작으면 왼쪽 서브트리에서 검색
+    if target < root.value: 
+#        return search_bet( 다음들어갈 값 , 찾는값)
+        return search_bst(root.left, target)
     ## target이 크면 오른쪽 서브트리에서 검색
     pass
+    if target> root.value: 
+        return search_bst(root.right, target)
+    
 
 # 테스트 케이스
 if __name__ == "__main__":
