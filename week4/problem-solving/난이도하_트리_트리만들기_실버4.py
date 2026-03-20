70b893e7bc1f08837ffb8efdc0a861732917c182
Cho1022 <cbs001022@gmail.com>
2026-03-20 21:21:44 +0900

    백준 14244-트리 만들기 close #85

--- week4/problem-solving/난이도하_트리_트리만들기_실버4.py
+++ week4/problem-solving/난이도하_트리_트리만들기_실버4.py
@@ -1,2 +1,17 @@
 # 트리 - 트리 만들기 (백준 실버4)
 # 문제 링크: https://www.acmicpc.net/problem/14244
+
+import sys
+input = sys.stdin.readline
+# 두 수를 넣고 n = 5 m = 2
+n , m = map(int, input().split())
+
+# n - m =  2로 0 - 1 - 2 까지  연결 
+for i in range(n - m):
+    print(i, i +_1) 
+#(0 ,1)
+#(1, 2) 이렇게 출력 하면 성공
+
+for j in range(n - m, n - 1):
+    print(j, j + 1)
+    
