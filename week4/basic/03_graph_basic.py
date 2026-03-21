d2bf3f2ab7a52f5f95a411a0e1d660fdf599973f
Cho1022 <cbs001022@gmail.com>
2026-03-21 17:30:56 +0900

    무방향 그래프 인접 행렬 close #80

--- week4/basic/03_graph_basic.py
+++ week4/basic/03_graph_basic.py
@@ -42,12 +42,16 @@ def create_graph(vertices, edges, directed=False):
     """
     # TODO: 빈 그래프 초기화
     pass
-    
+    graph = {}  
+    for i in range(vertices):
+        graph[i] = []
     # TODO: 간선 추가
     ## 간선 추가 (u에서 v로)
+    for u , v in edges: 
+        graph[u].append(v) 
     ## 무방향 그래프면 반대 방향도 추가
-    pass
-    
+        if directed == False: 
+            graph[v].append(u)
     return graph
 
 # 테스트 케이스
